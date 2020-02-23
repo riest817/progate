300e97127d14f87207aae38b895c8508310d2cb9
riest817 <sano817@hotmail.co.jp>
2020-02-23 19:19:06 +0900

    first commit

---
+++ Ruby on Rails/rails5_study_5/tweet_app/db/.backup.sql
@@ -0,0 +1,15 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20170331045923');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2017-03-31 05:21:00.324397','2017-03-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'次は何の本を読もうかな。','2017-03-31 05:24:15.529063','2017-03-31 05:24:15.529063');
+INSERT INTO posts VALUES(2,'今日のランチおいしかった。','2017-03-31 05:24:32.004378','2017-03-31 05:24:32.004378');
+INSERT INTO posts VALUES(5,'たまには復習もしようかな！','2020-02-04 16:29:19.227816','2020-02-04 16:29:19.227816');
+INSERT INTO posts VALUES(6,'a','2020-02-04 17:06:04.761878','2020-02-04 17:06:04.761878');
+INSERT INTO posts VALUES(7,'gigui','2020-02-04 17:12:08.540181','2020-02-04 17:12:08.540181');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',7);
+COMMIT;
